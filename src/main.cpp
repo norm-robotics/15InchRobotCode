74cebd54e3cae9ce27eb2ea6e23a5390f0531d21
Anthony DiBernardo <adibern2@charlotte.edu>
2026-01-22 22:50:00 -0500

    Added driving and intake code, also created trim controls

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <format>
 
 /**
  * A callback function for LLEMU's center button.
@@ -75,20 +76,66 @@ void autonomous() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::MotorGroup left_mg({-1, -2, -3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup right_mg({11, 12, 13});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+    pros::MotorGroup gameSystem({19, 20});
 
 
+    double forTrim = 0.6;
+    double latTrim = 0.5;
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
+
+        master.set_text(1, 0, std::format("Trim for:{0:.0f}|lat:{1:.0f}",forTrim*100,latTrim*100));
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)){
+            latTrim+=0.05;
+            if(latTrim >= 100){
+                latTrim = 100;
+            }
+            if(latTrim <= 0){
+                latTrim = 0;
+            }
+        }
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)){
+            latTrim-=0.05;
+            if(latTrim >= 100){
+                latTrim = 100;
+            }
+            if(latTrim <= 0){
+                latTrim = 0;
+            }
+        }
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)){
+            forTrim+=0.05;
+            if(latTrim >= 100){
+                forTrim = 100;
+            }
+            if(latTrim <= 0){
+                forTrim = 0;
+            }
+        }
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)){
+            forTrim-=0.05;
+            if(latTrim >= 100){
+                forTrim = 100;
+            }
+            if(latTrim <= 0){
+                forTrim = 0;
+            }
+        }
 		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
+		int dir = forTrim*(master.get_analog(ANALOG_LEFT_Y));    // Gets amount forward/backward from left joystick
+		int turn = -latTrim*(master.get_analog(ANALOG_LEFT_X));  // Gets the turn left/right from left joystick
 		left_mg.move(dir - turn);                      // Sets left motor voltage
 		right_mg.move(dir + turn);                     // Sets right motor voltage
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_A)){
+            gameSystem.move(127);
+        } else { 
+            gameSystem.move(0);
+        }
 		pros::delay(20);                               // Run for 20 ms then update
 	}
 }
